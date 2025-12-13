9d7519b2d0053c369638bf6e4cc6287788786e41
V-Karch <luna.karch05@gmail.com>
2025-12-12 21:09:57 -0500

    cookie.py: autoformatted with black

--- cookie.py
+++ cookie.py
@@ -1,16 +1,21 @@
 import browser_cookie3
 
-def find_libro_fm_cookies() -> dict | None:
+
+def find_libro_fm_cookies(verbose: bool = False) -> dict | None:
     found_cookies = {}
     for browser in browser_cookie3.all_browsers:
-        try: # This can fail if the browser isn't present
+        try:  # This can fail if the browser isn't present
             for cookie in browser(domain_name="libro.fm"):
                 found_cookies[cookie.name] = cookie.value
-            
-            print(f"Found https://libro.fm/ cookies in browser {browser.__name__}")
+
+            if verbose:
+                print(f"Found https://libro.fm/ cookies in browser {browser.__name__}")
             return found_cookies
         except browser_cookie3.BrowserCookieError:
-            print(f"Failed to find https://libro.fm/ cookies with browser {browser.__name__}")
+            if verbose:
+                print(
+                    f"Failed to find https://libro.fm/ cookies with browser {browser.__name__}"
+                )
             continue
-        
+
     return None
